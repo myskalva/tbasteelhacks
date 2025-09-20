9f3583f491330af25f93a3380990c91afedf882b
myskalva <kittencat3454@gmail.com>
2025-09-20 14:34:57 -0400

    removed pygame docs code and implemented scene classes in main

--- steelhacksgame/scenes/world.py
+++ steelhacksgame/scenes/world.py
@@ -1,15 +1,18 @@
 import pygame
 
-class World:
+from scenes.scene import Scene
+
+class World(Scene):
     def __init__(self):
         self.player_pos = [400, 300]
+
     def update(self, dt, keys):
-    # move player
         speed = 200
         if keys[pygame.K_w]: self.player_pos[1] -= speed * dt
         if keys[pygame.K_s]: self.player_pos[1] += speed * dt
         if keys[pygame.K_a]: self.player_pos[0] -= speed * dt
         if keys[pygame.K_d]: self.player_pos[0] += speed * dt
+
     def draw(self, screen):
         screen.fill("black")
-        pygame.draw.circle(screen, "red", self.player_pos, 20) 
+        pygame.draw.circle(screen, "red", self.player_pos, 20)
