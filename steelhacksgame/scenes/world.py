b75e41c99e520a8490949f30364c0e1704cb566a
myskalva <kittencat3454@gmail.com>
2025-09-20 15:32:21 -0400

    world class issues :(

--- steelhacksgame/scenes/world.py
+++ steelhacksgame/scenes/world.py
@@ -1,18 +1,17 @@
 import pygame
-
 from scenes.scene import Scene
+from characters.player import Player  # make sure you have this class
 
 class World(Scene):
+    player = Player()
     def __init__(self):
-        self.player_pos = [400, 300]
+        super().__init__()
+        self.player = Player([400, 300], "assets/player1.png")  # your image
+        self.all_sprites = pygame.sprite.Group(self.player)
 
     def update(self, dt, keys):
-        speed = 200
-        if keys[pygame.K_w]: self.player_pos[1] -= speed * dt
-        if keys[pygame.K_s]: self.player_pos[1] += speed * dt
-        if keys[pygame.K_a]: self.player_pos[0] -= speed * dt
-        if keys[pygame.K_d]: self.player_pos[0] += speed * dt
+        self.all_sprites.update(dt, keys)
 
     def draw(self, screen):
         screen.fill("black")
-        pygame.draw.circle(screen, "red", self.player_pos, 20)
+        self.all_sprites.draw(screen)
