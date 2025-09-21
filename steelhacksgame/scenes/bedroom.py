178153ee772de016ad08573be34e55aee6ca4653
skrocks <skrocks@Shirins-MacBook-Air.local>
2025-09-20 21:12:01 -0400

    Door collision

--- steelhacksgame/scenes/bedroom.py
+++ steelhacksgame/scenes/bedroom.py
@@ -13,14 +13,18 @@ class Bedroom(Scene):
         #initializing the chracter
         self.player = Player([400, 300], "assets/player1.png")  # your image
         self.all_sprites = pygame.sprite.Group(self.player)
+        self.door_rect = pygame.Rect(130,150,120,175)
     
     #calls update so player can move around with the keys
     def update(self, dt, keys):
         self.all_sprites.update(dt, keys)
+        if self.player.rect.colliderect(self.door_rect):
+            print("Yayyyyyyy")
 
     #creates a black box
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
         pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 2)
+        pygame.draw.rect(screen, (255,0,0), self.door_rect,2)
         self.all_sprites.draw(screen)
 
